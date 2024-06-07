f1376c02a67db9bb8e3e8082e1b357648d82dcaa
benja <benj.candia@duocuc.cl>
2024-06-07 11:34:28 -0400

    ejercicio 1 listo

---
+++ csv_con_python.py
@@ -0,0 +1,19 @@
+import csv
+
+listnombre = []
+
+with open ("nombres.csv", "w",newline="") as archivo:
+    escribir = csv.writer(archivo)
+    for j in range (3):
+        nombre = input("Ingrese nombre")
+        listnombre.append(nombre)
+        escribir.writerow(listnombre)
+
+def nombrelargo(p_lista):
+    for i in range (3):
+        if i==0:
+            nombrelargo = p_lista[0]
+        else:
+            if len(p_lista[i]) > len(nombrelargo):
+                nombrelargo = p_lista[i]
+    print ("El nombre mas largo es ", nombrelargo)
